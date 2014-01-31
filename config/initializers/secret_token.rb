a9ac012f5ef517ae0db7fc17790e8c785a073869
christopher nguyen <christoph.nguyen@gmail.com>
2014-01-30 23:07:10 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Crowdpix::Application.config.secret_key_base = 'ccf2188caf489e4d70171ef69f24155cb79a35843a02455a9a86b422ebee5912799170a28dcc813176127040779f851d8e423b45a79462745ce3234046b7d6f2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Crowdpix::Application.config.session_store :cookie_store, key: '_crowdpix_session'
